0042f8e96cd03d4707b8285cd8e619bb9962a4c5
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-07-01 22:51:20 +0500

    fix ui

--- src/pages/Room/components/CollabPanel.jsx
+++ src/pages/Room/components/CollabPanel.jsx
@@ -7,7 +7,7 @@ import ParticipantsList from "./ParticipantList";
 // Main Collaboration Panel Component
 function CollaborationPanel({ clients, socket, roomId, currentUsername }) {
     return (
-        <div className="p-4 max-w-screen  md:p-6 bg-[#1F2937] shadow-inner h-full flex flex-col w-full md:max-w-[300px]">
+        <div className="p-4 max-w-screen  md:p-6 bg-[#1F2937] shadow-inner h-full flex flex-col w-full md:max-w-[500px]">
             <h3 className="text-lg font-semibold mb-4 text-white border-b border-white/50 pb-2">
                 Collaboration Panel
             </h3>
